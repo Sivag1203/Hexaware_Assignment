1bdfe2f28fadf81980a5b831b68227d91bb175c3
Sivaganesh Natarajavel <sivaganeshnatarajavel@gmail.com>
2025-04-15 10:07:25 +0530

    changes made for tasks

--- Java Assignments/Task_11/src/main/java/org/example/service/ICustomerServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-public interface ICustomerServiceProvider {
-    float getAccountBalance(long accNo);
-    float deposit(long accNo, float amount);
-    float withdraw(long accNo, float amount);
-    void transfer(long fromAcc, long toAcc, float amount);
-    void getAccountDetails(long accNo);
-}
